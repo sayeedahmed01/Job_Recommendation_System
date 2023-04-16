9deaa258823b89c77b00144625e4bf0384809504
sayeedahmed01 <ahmed.say@northeastern.edu>
2023-04-15 22:23:32 -0400

    Modified the scrapeIndeed method to handle content not loading

--- jobscapper/src/main/scala/IndeedScraperMulti.scala
+++ jobscapper/src/main/scala/IndeedScraperMulti.scala
@@ -23,26 +23,23 @@ object IndeedScraperMulti {
     print("Number of pages: ")
     val numPages = scanner.nextInt()
     scanner.close()
-    val locations = List("Austin")//, "Boston")//, "Seattle", "Chicago", "Austin", "San Jose","Boulder", "Washington")
+    val locations = List("Austin", "San Jose")//"Boston", "Seattle", "Chicago", "Austin", "San Jose","Boulder", "Washington"
 
     val csvFile = new File(s"$query+all_locations.csv")
-    val writer = new FileWriter(csvFile, true) // Open file in append mode
+    val writer = new FileWriter(csvFile, true)
     writer.write("Job ID|Job Title|Company|Location|Job Link|Salary|Job Description|Date Posted\n")
 
     val options = new ChromeOptions()
     options.addArguments("headless")
 
-    // Setup WebDriver only once
     WebDriverManager.chromedriver().setup()
 
-    // Define a custom ExecutionContext for parallelism
     val executor = Executors.newFixedThreadPool(locations.size)
     implicit val ec: ExecutionContext = ExecutionContext.fromExecutor(executor)
 
-    // Use Future.sequence to wait for all Futures to complete
     val scrapingFutures = locations.map { location =>
       Future {
-        val driver = new ChromeDriver(options)
+        val driver = new ChromeDriver()//(options)
         val jobs = scrapeIndeed(driver, query, location, numPages)
         driver.quit()
         (location, jobs)
@@ -51,10 +48,8 @@ object IndeedScraperMulti {
 
     val allJobs = Await.result(Future.sequence(scrapingFutures), 60.minutes)
 
-    // Write the jobs to the CSV file
     allJobs.foreach { case (_, jobs) => writeJobsToCSV(writer, jobs) }
 
-    // Shutdown the thread pool executor
     executor.shutdown()
 
     writer.close()
@@ -62,13 +57,13 @@ object IndeedScraperMulti {
 
   def scrapeIndeed(driver: WebDriver, query: String, location: String, numPages: Int): List[Job] = {
     val searchResultURLs = (0 until numPages * 10 by 10).map(start => s"https://www.indeed.com/jobs?q=$query&l=$location&sort=date&start=$start").toList
-    val wait = new WebDriverWait(driver, Duration.ofSeconds(60).toMillis)
+    val wait = new WebDriverWait(driver, Duration.ofSeconds(30).toMillis)
 
     searchResultURLs.flatMap { url =>
       driver.get(url)
-      val results = wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.className("resultContent")))
-      results.asScala.map { result =>
-        val parsedResult = Jsoup.parse(result.getAttribute("outerHTML"))
+      val results = retry(() => driver.findElements(By.className("resultContent")), 3, 5000)
+      results.asScala.toList.map { result =>
+        val parsedResult = Jsoup.parse(retry(() => result.getAttribute("outerHTML"), 3, 5000))
         val jobLink = parsedResult.select("a").attr("href")
         val description = getJobDescription(driver, wait, jobLink)
         Job(
@@ -81,24 +76,47 @@ object IndeedScraperMulti {
           description = description.getOrElse(""),
           datePosted = parseDatePosted(parsedResult)
         )
-      }.toList
+      }
     }
+  }
+
+  def retryFindElements[T](operation: => T, retries: Int): T = {
+    try {
+      operation
+    } catch {
+      case e: Exception if retries > 0 =>
+        Thread.sleep(5000)
+        retryFindElements(operation, retries - 1)
+    }
+  }
+
+  def retry[T](operation: () => T, retries: Int, delay: Long): T = {
+    var attempts = 0
+    while (attempts < retries) {
+      try {
+        return operation()
+      } catch {
+        case e: Throwable =>
+          attempts += 1
+          if (attempts < retries) {
+            Thread.sleep(delay)
+          } else {
+            throw e
+          }
+      }
+    }
+    throw new RuntimeException("This should never happen.") // This line should never be reached.
   }
 
   def getJobDescription(driver: WebDriver, wait: WebDriverWait, jobLink: String): Option[String] = {
     Try {
-      // Open job link in a new tab
       val script = s"window.open('$jobLink', '_blank');"
       driver.asInstanceOf[ChromeDriver].executeScript(script)
-
-      // Wait for 2 seconds
       Thread.sleep(2000)
 
-      // Switch to the new tab
       val newTabHandle = driver.getWindowHandles.asScala.toList.last
       driver.switchTo().window(newTabHandle)
 
-      // Extract job description
       val result = Try(wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[contains(@class, 'jobsearch-JobComponent-description') or contains(@class, 'jobsearch-jobDescriptionText')]")))).recover {
         case _: TimeoutException => null
       }.get
@@ -110,10 +128,8 @@ object IndeedScraperMulti {
         throw new Exception("Timeout while waiting for job description")
       }
 
-      // Wait for 2 seconds
       Thread.sleep(2000)
 
-      // Close the new tab and switch back to the main window
       driver.close()
       driver.switchTo().window(driver.getWindowHandles.asScala.toList.head)
 
@@ -123,8 +139,6 @@ object IndeedScraperMulti {
       case Failure(_) => None
     }
   }
-
-
   def parseSalary(parsedResult: org.jsoup.nodes.Document): String = {
     val salarySnippet = parsedResult.select("div.metadata.salary-snippet-container")
     if (salarySnippet != null) salarySnippet.text
